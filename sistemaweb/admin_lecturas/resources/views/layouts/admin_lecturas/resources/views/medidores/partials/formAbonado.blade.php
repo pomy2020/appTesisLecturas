7184e3dcc827ba93dbab0bfc8f7443a9c62d76af
Pedro <pmejia1979@hotmail.com>
2021-01-19 10:28:24 -0500

    actualizacion de layouts
    actualización del archivo layouts , para darle color

--- sistemaweb/admin_lecturas/resources/views/layouts/admin_lecturas/resources/views/medidores/partials/formAbonado.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="form-group">
-	{{ Form::hidden('id', null, ['class' => 'form-control']) }}
-</div>
-<div class="form-group">
-	{{ Form::label('Cédula', 'Cédula') }}
-	{{ Form::text('cedula', null, ['class' => 'form-control']) }}
-	{{ Form::submit('Buscar', ['class' => 'btn btn-sm btn-primary']) }}
-</div>
--- sistemaweb/admin_lecturas/resources/views/layouts/app.blade.php
+++ sistemaweb/admin_lecturas/resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body style="background-color:red;">
+
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
@@ -34,7 +35,8 @@
                 </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
+                     <!-- Collapsed Hamburger -->
+                   
                     <ul class="nav navbar-nav">
                         @can('personas.index')
                         <li class="nav-item">
@@ -59,6 +61,16 @@
                         @can('roles.index')
                         <li class="nav-item">                            
                             <a class="nav-link" href="{{ route('roles.index') }}">Roles</a>
+                        </li>
+                        @endcan
+                        @can('provincia.index')
+                        <li class="nav-item">                            
+                            <a class="nav-link" href="{{ route('provincia.index') }}">provincias</a>
+                        </li>
+                        @endcan
+                         @can('politica.index')
+                        <li class="nav-item">                            
+                            <a class="nav-link" href="{{ route('politica.index') }}">politicas</a>
                         </li>
                         @endcan
                     </ul>
